8fa38ba0d0cf2b2b397246acd7062392462af39d
ritartha <ritartha@gmail.com>
2026-03-01 12:43:42 +0530

    backend and files organised

---
+++ backend/nibl_project/wsgi.py
@@ -0,0 +1,9 @@
+"""WSGI config for nibl_project."""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nibl_project.settings')
+
+application = get_wsgi_application()
